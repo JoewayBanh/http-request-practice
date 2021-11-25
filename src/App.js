610b9d978157cbd3d05aca03e377ef5393a21a7c
Joeway Banh <joewaybanh@hotmail.com>
2021-11-25 19:22:55 +0100

    cleaned up the return in App.js with conditionals prior

--- src/App.js
+++ src/App.js
@@ -14,11 +14,13 @@ function App() {
 
     try {
       const res = await fetch("https://swapi.dev/api/films");
+
       if (!res.ok) {
+        //.ok returns a bool if response was successful
         throw new Error("Something went wrong, buddy");
       }
 
-      const data = await res.json(); //her behandles json til js object
+      const data = await res.json(); //her parses JSON til js object
 
       const transformedMovies = data.results.map((movieData) => {
         return {
@@ -34,16 +36,23 @@ function App() {
     }
     setIsLoading(false);
   }
+
+  let content = <p>No movies found.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  } else if (error) {
+    content = { error };
+  }
+  if (isLoading) {
+    content = <p>Loading movies...</p>;
+  }
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {isLoading ? "Loading movies.." : <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error && <p>No movies found.</p>}
-        {!isLoading && error && <p>{error}</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
